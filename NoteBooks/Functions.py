7d059e005d2411b164913af63e22427d51e87a96
Parker <pharalds11@gmail.com>
2024-11-21 10:52:25 -0800

    Changed functions notebook to a .py file

---
+++ NoteBooks/Functions.py
@@ -0,0 +1,22 @@
+#unscaled linear model
+def linear_model(df,dependant,features):
+    y = df[dependant]
+    X = df[features]
+    X = sm.add_constant(X)
+
+    model = sm.OLS(y, X) 
+    results = model.fit()
+    print(results.summary())
+
+
+#scaled linear model
+def linear_model_scaled(df,dependant,features):
+    y = df[dependant]
+    X = df[features]
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X) 
+    X_scaled = sm.add_constant(X_scaled)
+
+    model = sm.OLS(y, X_scaled) 
+    results = model.fit()
+    print(results.summary())
