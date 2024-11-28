40ac8423720ff9a5874f4c2096855d9277131ca7
Parker <pharalds11@gmail.com>
2024-11-28 11:30:42 -0800

    Adjusted where some functions were located in workbooks

--- NoteBooks/utils.py
+++ NoteBooks/utils.py
@@ -1,28 +1,59 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import statsmodels.api as sm
-from sklearn.preprocessing import StandardScaler
-
-#unscaled linear model
-def linear_model(df,dependant,features):
-    y = df[dependant]
-    X = df[features]
-    X = sm.add_constant(X)
-
-    model = sm.OLS(y, X) 
-    results = model.fit()
-    print(results.summary())
-
-
-#scaled linear model
-def linear_model_scaled(df,dependant,features):
-    y = df[dependant]
-    X = df[features]
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X) 
-    X_scaled = sm.add_constant(X_scaled)
-
-    model = sm.OLS(y, X_scaled) 
-    results = model.fit()
-    print(results.summary())
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def linear_model(df,dependant,features):\n",
+    "    y = df[dependant]\n",
+    "    X = df[features]\n",
+    "    X = sm.add_constant(X)\n",
+    "\n",
+    "    model = sm.OLS(y, X) \n",
+    "    results = model.fit()\n",
+    "    print(results.summary())"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def linear_model_scaled(df,dependant,features):\n",
+    "    y = df[dependant]\n",
+    "    X = df[features]\n",
+    "    scaler = StandardScaler()\n",
+    "    X_scaled = scaler.fit_transform(X) \n",
+    "    X_scaled = sm.add_constant(X_scaled)\n",
+    "\n",
+    "    model = sm.OLS(y, X_scaled) \n",
+    "    results = model.fit()\n",
+    "    print(results.summary())"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "first_env",
+   "language": "python",
+   "name": "first_env"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.15"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
