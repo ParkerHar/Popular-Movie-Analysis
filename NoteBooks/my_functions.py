d1e7997798b6871529a66542bf5edef0eab11843
Parker <pharalds11@gmail.com>
2024-11-28 11:54:43 -0800

    Comments added to model

--- NoteBooks/my_functions.py
+++ NoteBooks/my_functions.py
@@ -1,9 +1,16 @@
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 import statsmodels.api as sm
 
 def linear_model(df,dependant,features):
+    """
+    Fits a linear regression model.
+    Args:
+        df (DataFrame): The dataset.
+        dependent (str): The dependent variable name.
+        features (list): A list of feature column names.
+    Returns:
+        OLS regression results.
+    """
     y = df[dependant]
     X = df[features]
     X = sm.add_constant(X) 
